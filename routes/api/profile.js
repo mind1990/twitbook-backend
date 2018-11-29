4c29bfafa50cc03b1ee0a5bcfd7f56978128207a
Tirapat <mind1990@gmail.com>
2018-11-28 16:34:01 -0800

    adds POST request for experience in profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -152,6 +152,27 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
 	}
 );
 
+// @route   POST api/profile/experience
+// @desc    Add experience to profile
+// @access  Private
+router.post('/experience', passport.authenticate('jwt', { session: false }), (req, res) => {
+	Profile.findOne({ user: req.user.id })
+	.then(profile => {
+		const newExp = {
+			title: req.body.title,
+			company: req.body.company,
+			location: req.body.location,
+			from: req.body.from,
+			to: req.body.to,
+			current: req.body.current,
+			description: req.body.description
+		}
+		// Add to exp array
+		profile.experience.unshift(newExp);
+		profile.save().then(profile => res.json(profile));
+	})
+})
+
 
 module.exports = router;
 
