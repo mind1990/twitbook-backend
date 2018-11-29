5135ad2cfe12839dc6e84fcfdaa89b9cdc61cc19
Tirapat <mind1990@gmail.com>
2018-11-28 16:57:32 -0800

    adds experience and education routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,7 +5,8 @@ const passport = require('passport');
 
 // Load Validation
 const validateProfileInput = require('../../validation/profile');
-
+const validateExperienceInput = require('../../validation/experience');
+const validateEducationInput = require('../../validation/education');
 // Load Profile Model
 const Profile = require('../../models/Profile');
 // Load User Profile
@@ -96,7 +97,66 @@ router.get('/user/:user_id', (req, res) => {
 // @desc    Create or edit user profiel
 // @access  Private
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-		const { errors, isValid } = validateProfileInput(req.body);
+	const { errors, isValid } = validateProfileInput(req.body);
+
+	// Check Validation
+	if (!isValid) {
+		// Return any errors with 400 status
+		return res.status(400).json(errors);
+	}
+
+	// Get fields
+	const profileFields = {};
+	profileFields.user = req.user.id;
+	if (req.body.handle) profileFields.handle = req.body.handle;
+	if (req.body.company) profileFields.company = req.body.company;
+	if (req.body.website) profileFields.website = req.body.website;
+	if (req.body.location) profileFields.location = req.body.location;
+	if (req.body.bio) profileFields.bio = req.body.bio;
+	if (req.body.status) profileFields.status = req.body.status;
+	if (req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+	// Skills - Split into array
+	if(typeof req.body.skills !== 'undefined') {
+		profileFields.skills = req.body.skills.split(',');
+	}
+	// Social
+	profileFields.social = {};
+	if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+	if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+	if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+	if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+	if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+	Profile.findOne({ user: req.user.id }).then(profile => {
+		if (profile) {
+			// Update
+			Profile.findOneAndUpdate(
+				{ user: req.user.id },
+				{ $set: profileFields },
+				{ new: true }
+			).then(profile => res.json(profile));
+		} else {
+			// Create
+
+			// Check if handle exists
+			Profile.findOne({ handle: profileFields.handle }).then(profiel => {
+				if (profile) {
+					errors.handle = 'That handle already exists';
+					res.status(400).json(errors);
+				}
+
+				// Save Profile
+				new Profile(profileFields).save().then(profile => res.json(profile));
+			});
+		}
+	});
+});
+
+// @route   POST api/profile/experience
+// @desc    Add experience to profile
+// @access  Private
+router.post('/experience', passport.authenticate('jwt', { session: false }), (req, res) => {
+	const { errors, isValid } = validateExperienceInput(req.body);
 
 		// Check Validation
 		if (!isValid) {
@@ -104,58 +164,6 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
 			return res.status(400).json(errors);
 		}
 
-		// Get fields
-		const profileFields = {};
-		profileFields.user = req.user.id;
-		if (req.body.handle) profileFields.handle = req.body.handle;
-		if (req.body.company) profileFields.company = req.body.company;
-		if (req.body.website) profileFields.website = req.body.website;
-		if (req.body.location) profileFields.location = req.body.location;
-		if (req.body.bio) profileFields.bio = req.body.bio;
-		if (req.body.status) profileFields.status = req.body.status;
-		if (req.body.githubusername) profileFields.githubusername = req.body.githubusername;
-		// Skills - Split into array
-		if(typeof req.body.skills !== 'undefined') {
-			profileFields.skills = req.body.skills.split(',');
-		}
-		// Social
-		profileFields.social = {};
-		if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
-		if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
-		if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
-		if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
-		if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
-
-		Profile.findOne({ user: req.user.id }).then(profile => {
-			if (profile) {
-				// Update
-				Profile.findOneAndUpdate(
-					{ user: req.user.id },
-					{ $set: profileFields },
-					{ new: true }
-				).then(profile => res.json(profile));
-			} else {
-				// Create
-
-				// Check if handle exists
-				Profile.findOne({ handle: profileFields.handle }).then(profiel => {
-					if (profile) {
-						errors.handle = 'That handle already exists';
-						res.status(400).json(errors);
-					}
-
-					// Save Profile
-					new Profile(profileFields).save().then(profile => res.json(profile));
-				});
-			}
-		});
-	}
-);
-
-// @route   POST api/profile/experience
-// @desc    Add experience to profile
-// @access  Private
-router.post('/experience', passport.authenticate('jwt', { session: false }), (req, res) => {
 	Profile.findOne({ user: req.user.id })
 	.then(profile => {
 		const newExp = {
@@ -170,9 +178,37 @@ router.post('/experience', passport.authenticate('jwt', { session: false }), (re
 		// Add to exp array
 		profile.experience.unshift(newExp);
 		profile.save().then(profile => res.json(profile));
-	})
-})
+	});
+});
 
+// @route   POST api/profile/education
+// @desc    Add education to profile
+// @access  Private
+router.post('/education', passport.authenticate('jwt', { session: false }), (req, res) => {
+	const { errors, isValid } = validateEducationInput(req.body);
+
+		// Check Validation
+		if (!isValid) {
+			// Return any errors with 400 status
+			return res.status(400).json(errors);
+		}
+
+	Profile.findOne({ user: req.user.id })
+	.then(profile => {
+		const newEdu = {
+			school: req.body.school,
+			degree: req.body.degree,
+			fieldofstudy: req.body.fieldofstudy,
+			from: req.body.from,
+			to: req.body.to,
+			current: req.body.current,
+			description: req.body.description
+		}
+		// Add to exp array
+		profile.education.unshift(newEdu);
+		profile.save().then(profile => res.json(profile));
+	});
+});
 
 module.exports = router;
 
