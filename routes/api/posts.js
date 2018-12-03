089a6b18bf94bad90e6d436512c9e7ca314e5edb
Tirapat <mind1990@gmail.com>
2018-12-02 19:26:00 -0800

    adds api/posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -31,7 +31,7 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
 	const newPost = new Post({
 		text: req.body.text,
 		name: req.body.name,
-		avatar: req.body.name,
+		avatar: req.body.avatar,
 		user: req.user.id
 	});
 
