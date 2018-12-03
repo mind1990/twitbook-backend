e9228ee8f37a28d4cdb2b5d9b13d48ad073717d6
Tirapat <mind1990@gmail.com>
2018-12-02 19:49:21 -0800

    adds a route to delete a post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,8 @@ const passport = require('passport');
 
 // Post model
 const Post = require('../../models/Post');
+// Profile model
+const Profile = require('../../models/Profile');
 
 
 // Validation
@@ -61,5 +63,28 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
 	newPost.save().then(post => res.json(post));
 });
 
+// Delete a post
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+	Profile.findOne({ user: req.user.id })
+	.then(profile => {
+		Post.findById(req.params.id)
+		.then(post => {
+			// Check for post owner
+			if (post.user.toString() !== req.user.id) {
+				return res.status(401).json({ notauthorized: 'User not authorized' });
+			}
+			// Delete
+			post.remove().then(() => res.json({ success: true }));
+		})
+		.catch(err => res.status(404).json({ postnotfound: 'No post found' }))
+	})
+})
+
 
 module.exports = router;
+
+
+
