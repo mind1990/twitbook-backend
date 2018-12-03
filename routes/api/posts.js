75b0e154ea3862e9708c909154ac28e023c47ec6
Tirapat <mind1990@gmail.com>
2018-12-02 21:03:37 -0800

    adds a DELETE route to remove a comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -157,6 +157,30 @@ router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (r
 	.catch(err => res.status(404).json({ postnotfound: 'No post found' }));
 });
 
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Remove a comment from a post
+// @access  Private
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+	Post.findById(req.params.id)
+	.then(post => {
+		// Check if the comment exists
+		if (post.comments.filter(comment => comment.id.toString() === req.params.comment_id).length === 0) {
+			return res.status(404).json({ commentnotexists: 'Comment does not exist' });
+		}
+		// Get remove index
+		const removeIndex = post.comments
+		.map(item => item._id.toString())
+		.indexOf(req.params.comment_id);
+
+		// Splice the comment out of array
+		post.comments.splice(removeIndex, 1);
+
+		// Save
+		post.save().then(post => res.json(post));
+	})
+	.catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+});
+
 
 
 module.exports = router;
