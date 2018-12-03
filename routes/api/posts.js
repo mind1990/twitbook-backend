ce475d0d33b2928d479d2ff0f89ccc41e79f1e9d
Tirapat <mind1990@gmail.com>
2018-12-02 19:32:55 -0800

    adds a route to get all the posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -16,6 +16,19 @@ const validatePostInput = require('../../validation/post');
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Posts works' }));
 
+
+// Anyone who's not loged in can see the posts
+// @route   GET api/posts
+// @desc    Get post
+// @access  Public
+router.get('/', (req, res) => {
+	Post.find()
+	.sort({date: -1})
+	.then(posts => res.json(posts))
+	.catch(err => res.status(404));
+});
+
+// Only people who are logged in can post stuffs
 // @route   POST api/posts
 // @desc    Create post
 // @access  Private
