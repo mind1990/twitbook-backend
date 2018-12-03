8dbd9d4be88982ff6855db712bf4e409c38d23bd
Tirapat <mind1990@gmail.com>
2018-12-02 20:45:10 -0800

    adds a POST route to add a comment to a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -92,7 +92,7 @@ router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req,
 			// Add user id to likes array
 			post.likes.unshift({ user: req.user.id });
 
-			// Save to the database
+			// Save
 			post.save().then(post => res.json(post));
 		})
 		.catch(err => res.status(404).json({ postnotfound: 'No post found' }));
@@ -122,15 +122,50 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
 			// Splice out of array
 			post.likes.splice(removeIndex, 1);
 
-			// Save to database
+			// Save
 			post.save().then(post => res.json(post));
 		})
 		.catch(err => res.status(404).json({ postnotfound: 'No post found' }));
 	});
 });
 
+// @route   POST api/posts/comment/:id
+// @desc    Add comment to post
+// @access  Private
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+	const { errors, isValid } = validatePostInput(req.body);
+
+	// Check validation
+	if (!isValid) {
+		// If any errors, send 400 with errors object
+		return res.status(400).json(errors);
+	}
+
+	Post.findById(req.params.id)
+	.then(post => {
+		const newComment = {
+			text: req.body.text,
+			name: req.body.name,
+			avatar: req.body.avatar,
+			user: req.user.id
+		}
+		// Add to comments array
+		post.comments.unshift(newComment);
+		// Save
+		post.save().then(post =>res.json(post))
+	})
+	.catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+});
+
+
 
 module.exports = router;
+
+
+
+
+
+
 
 
 
